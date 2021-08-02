e48ac92cd481edc89b95abd042ac05e5001677b6
simba-fs <simba.fs@gmail.com>
2021-08-02 22:51:56 +0800

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	app := gin.Default()
+	app.Any("/", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "ok",
+		})
+	})
+}
